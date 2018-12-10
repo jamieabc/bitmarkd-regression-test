7a76d178b29b10b5f39e6c125a4f6592ad67a19e
Aaron Chang <xesuschang@gmail.com>
2018-12-10 17:57:41 +0800

    remove variable $HOME for jenkins mis-replace

--- features/jenkins.sh
+++ features/jenkins.sh
@@ -1,7 +1,7 @@
 shared_dir="jenkins"
 regression_env_script="bin/run-regression"
 regression_dir="bitmarkd-regression-test"
-result_file="${HOME}/${shared_dir}/result.json"
+result_file="${shared_dir}/result.json"
 repo="https://git.bitmark.com:8080/system/bitmarkd-regression-test.git"
 conf="conf/cli.conf"
 
@@ -11,7 +11,7 @@ ERROR_CODE=1
 export PATH=~/gocode/bin:$PATH
 
 echo removing previous result
-rm ${result_file}
+rm ~/${result_file}
 
 echo run regression environment setup
 eval ~/$regression_env_script
@@ -28,18 +28,18 @@ cp ~/${conf} ~/${regression_dir}/
 # run test cases
 cd ~/${regression_dir}
 echo running cucumber...
-cucumber --format json -o ${result_file}
+cucumber --format json -o ~/${result_file}
 
 # check cucumber status
 if [ $? -ne 0 ]; then
     cucumber_fail="true"
 fi
 
-if [ ! -f $result_file ]; then
+if [ ! -f "~/${result_file}" ]; then
     printf "${result_file} not exist, abort..."
     exit $ERROR_CODE
 else
-    cat ${result_file}
+    cat ~/${result_file}
 fi
 
 if [ "${cucumber_fail}" == "true" ]; then
