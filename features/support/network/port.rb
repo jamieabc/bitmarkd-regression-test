e6651c4c254e7b80be56a6a2d85120c103558069
Aaron Chang <xesuschang@gmail.com>
2019-12-20 11:02:09 +0800

    use same port for ci and local environment

--- features/support/network/port.rb
+++ features/support/network/port.rb
@@ -7,20 +7,16 @@ module Network
 
     def initialize(index)
       @node_index = index
-      @default_rpc = "2#{node_index}31"
-      @default_node = "2#{node_index}30"
-    end
-
-    def ci_port_prefix
-      "2"
+      @default_rpc = "22#{node_index}31"
+      @default_node = "22#{node_index}30"
     end
 
     def rpc
-      run_ci? ? "#{ci_port_prefix}#{default_rpc}" : default_rpc
+      default_rpc
     end
 
     def node
-      run_ci? ? "#{ci_port_prefix}#{default_node}" : default_node
+      default_node
     end
   end
 end
